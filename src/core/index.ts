219c81cc4829879d26cbcd1928a0bab043b7093e
Jose Ivan <jigonzalez930209@gmail.com>
2026-01-01 17:01:06 -0300

    refactor: remove PlotArea and CursorState type exports from core index.

--- src/core/index.ts
+++ src/core/index.ts
@@ -5,6 +5,6 @@
 export { createChart, type Chart, type ChartOptions, type ExportOptions } from './Chart';
 export { Series } from './Series';
 export { EventEmitter } from './EventEmitter';
-export { OverlayRenderer, type PlotArea, type CursorState } from './OverlayRenderer';
+export { OverlayRenderer } from './OverlayRenderer';
 export { InteractionManager } from './InteractionManager';
 export * from './annotations';
